11d0ca99cede3a1f8ce19cd149bddbf265d86d48
Cherie Davis <cheriedavis126@gmail.com>
2025-01-17 18:11:59 -0500

    badges working and code fully functioning.

--- Develop/index.js
+++ Develop/index.js
@@ -1,141 +1,75 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
 import fs from 'fs';
+import { generateMarkdown } from './utils/generateMarkdown.js';
 
-
-// TODO: Create an array of questions for user input
+// Array of questions for user input
 const questions = [
-{
-    section: "Title",
-    questions: [ 
-        "What is your project title"
-    ]
-},
-{
-    section: "Description",
-    questions: [
-        "Please provide a short description of your project.",
-        "What was your motivation for creating the project?",
-        "Why did you build this project?",
-        "What problem does this project solve?",
-        "What did you learn when creating this project?",
-    ]
-},
-/////table of contents section? Do I need to create a function that maps out the rest of the data?
-{
-    section: "Repository",
-    questions: [
-        "What is the link to your GitHub Repository?"
-    ]
-},
-{
-    section: "Installation",
-    questions: [
-        "What tools or packages did you install to complete this project?"
-    ]
-},
-{
-    section: "Usage",
-    questions: [
-        "Please provide instructions and / or examples for how to best use this project?"
-    ]
-},
-{
-    section: "Contributing",
-    questions: [
-      "Were there any collaborators on this project?"
-    ]
-  },
-{
-    section: "License",
-    questions: [
-        "What license did you use to complete this project?"
-    ]
-},
-/////insert badges????
-{
-    section: "Tests",
-    questions: [
-      "Did you run any tests for your project or application?"
-    ]
-  },
-{
-    section: "Questions",
-    questions: [
-        "What features are included in this project?"
-    ]
-}
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your project title?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project:',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'Apache 2.0', 'GPL v3', 'BSD 3-Clause', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What tools or packages did you install to complete this project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide instructions and/or examples for using this project:',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Were there any collaborators on this project?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Did you run any tests for your project or application?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
 ];
 
-///need to be able to nest the arrat for easy user output
-const allQuestions = questions.flatMap(section => {
-    return section.questions.map(question => ({
-      type: 'input',
-      name: question,
-      message: question
-    }));
-  });
-  
-/// Prompt to use inquirer
-inquirer.prompt(allQuestions).then((answers) => {
-    const readmeContent = generateREADME(answers);
+// Prompt the user with inquirer
+inquirer.prompt(questions).then((answers) => {
+  const readmeContent = generateMarkdown(answers); // Generate the README content with the badge
 
-    // Create a filename based on the project title
-    const title = answers['What is your project title'];
-    const fileName = title.toLowerCase().replace(/ /g, '-') + '.md'; // Create a filename from the title
+  // Create a filename based on the project title
+  const fileName = answers.title.toLowerCase().replace(/ /g, '-') + '.md';
 
-    writeToFile(fileName, readmeContent);
+  writeToFile(fileName, readmeContent); // Write the content to a file
 });
 
-
-// TODO: Create a function to write README file
+// Function to write README file
 function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, (err) => {
-          if (err) {
-            console.error("Error writing to file", err);
-          } else {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing to file', err);
+        } else {
             console.log(`${fileName} has been generated successfully!`);
-          }
-        });
-      }
-
-      function generateREADME(answers) {
-        return `
-      # ${answers['What is your project title']} 
-      
-      ## Description
-      - **Motivation**: ${answers['What was your motivation for creating the project?']}
-      - **Why build this project**: ${answers['Why did you build this project?']}
-      - **Problem solved**: ${answers['What problem does this project solve?']}
-      - **What I learned**: ${answers['What did you learn when creating this project?']}
-      
-      ## Installation
-      - ${answers['What tools or packages did you install to complete this project?']}
-      
-      ## Usage
-      - ${answers['Please provide instructions and / or examples for how to best use this project?']}
-      
-      ## Contributing
-      - Collaborators: ${answers['Were there any collaborators on this project?']}
-      
-      ## License
-      - License used: ${answers['What license did you use to complete this project?']}
-      
-      ## Tests
-      - ${answers['Did you run any tests for your project or application?']}
-      
-      ## Questions
-      - Features: ${answers['What features are included in this project?']}
-        `;
-      }
-
-
-      
-// TODO: Create a function to initialize app
-function initApp() {
-    console.log("Initializing the app!");
-  }
-  
-  //Function call to actually run the app
-  initApp()
-
+        }
+    });
+}
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,72 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+// Function to return a license badge based on the selected license
+export function renderLicenseBadge(license) {
+  if (license === 'None') return '';
+  return `![License](https://img.shields.io/badge/License-${encodeURIComponent(license)}-blue.svg)`;
 }
 
-export default generateMarkdown;
+// Function to return a license link
+export function renderLicenseLink(license) {
+  if (license === 'None') return '';
+  switch (license) {
+      case 'MIT':
+          return '[MIT License](https://opensource.org/licenses/MIT)';
+      case 'Apache 2.0':
+          return '[Apache 2.0 License](https://opensource.org/licenses/Apache-2.0)';
+      case 'GPL v3':
+          return '[GPL v3 License](https://www.gnu.org/licenses/gpl-3.0)';
+      case 'BSD 3-Clause':
+          return '[BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)';
+      default:
+          return '';
+  }
+}
+
+// Function to return the license section of the README
+export function renderLicenseSection(license) {
+  if (license === 'None') {
+      return '## License\nThis project is not licensed.';
+  }
+  return `## License\nThis application is covered under the ${renderLicenseLink(license)}.`;
+}
+
+// Function to generate markdown content for the README
+export function generateMarkdown(data) {
+  const licenseBadge = renderLicenseBadge(data.license); // Generates the badge
+  const licenseSection = renderLicenseSection(data.license); // Generates the license section
+
+  return `
+${licenseBadge} <!-- License badge is placed here -->
+
+# ${data.title}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributing
+${data.contributing}
+
+${licenseSection}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, please reach out:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: ${data.email}
+  `;
+}
