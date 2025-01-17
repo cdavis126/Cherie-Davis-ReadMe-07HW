a03ee2e2e8afa0ca94775c495a6e09ac0a7fbbdb
Cherie Davis <cheriedavis126@gmail.com>
2025-01-17 17:15:42 -0500

    not sure what's changing

--- Develop/index.js
+++ Develop/index.js
@@ -1,22 +1,7 @@
 // TODO: Include packages needed for this application
 import inquirer from 'inquirer';
-const inquirer = require('inquirer');
-const questions = require('./questions');
+import fs from 'fs';
 
-///need to be able to nest the arrat for easy user input
-const allQuestions = questions.flatMap(section => {
-    return section.questions.map(question => ({
-      type: 'input',
-      name: question,
-      message: question
-    }));
-  });
-
-  inquirer.prompt(allQuestions)
-  .then(answers => {
-    console.log("Your responses:", answers);
-  })
-  .catch(err => console.error(err));
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -36,6 +21,13 @@ const questions = [
         "What did you learn when creating this project?",
     ]
 },
+/////table of contents section? Do I need to create a function that maps out the rest of the data?
+{
+    section: "Repository",
+    questions: [
+        "What is the link to your GitHub Repository?"
+    ]
+},
 {
     section: "Installation",
     questions: [
@@ -49,24 +41,101 @@ const questions = [
     ]
 },
 {
+    section: "Contributing",
+    questions: [
+      "Were there any collaborators on this project?"
+    ]
+  },
+{
     section: "License",
     questions: [
         "What license did you use to complete this project?"
     ]
 },
+/////insert badges????
 {
-    section: "Features",
+    section: "Tests",
+    questions: [
+      "Did you run any tests for your project or application?"
+    ]
+  },
+{
+    section: "Questions",
     questions: [
         "What features are included in this project?"
     ]
 }
 ];
 
+///need to be able to nest the arrat for easy user output
+const allQuestions = questions.flatMap(section => {
+    return section.questions.map(question => ({
+      type: 'input',
+      name: question,
+      message: question
+    }));
+  });
+  
+/// Prompt to use inquirer
+inquirer.prompt(allQuestions).then((answers) => {
+    const readmeContent = generateREADME(answers);
+
+    // Create a filename based on the project title
+    const title = answers['What is your project title'];
+    const fileName = title.toLowerCase().replace(/ /g, '-') + '.md'; // Create a filename from the title
+
+    writeToFile(fileName, readmeContent);
+});
+
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+        fs.writeFile(fileName, data, (err) => {
+          if (err) {
+            console.error("Error writing to file", err);
+          } else {
+            console.log(`${fileName} has been generated successfully!`);
+          }
+        });
+      }
 
+      function generateREADME(answers) {
+        return `
+      # ${answers['What is your project title']} 
+      
+      ## Description
+      - **Motivation**: ${answers['What was your motivation for creating the project?']}
+      - **Why build this project**: ${answers['Why did you build this project?']}
+      - **Problem solved**: ${answers['What problem does this project solve?']}
+      - **What I learned**: ${answers['What did you learn when creating this project?']}
+      
+      ## Installation
+      - ${answers['What tools or packages did you install to complete this project?']}
+      
+      ## Usage
+      - ${answers['Please provide instructions and / or examples for how to best use this project?']}
+      
+      ## Contributing
+      - Collaborators: ${answers['Were there any collaborators on this project?']}
+      
+      ## License
+      - License used: ${answers['What license did you use to complete this project?']}
+      
+      ## Tests
+      - ${answers['Did you run any tests for your project or application?']}
+      
+      ## Questions
+      - Features: ${answers['What features are included in this project?']}
+        `;
+      }
+
+
+      
 // TODO: Create a function to initialize app
-function init() {}
+function initApp() {
+    console.log("Initializing the app!");
+  }
+  
+  //Function call to actually run the app
+  initApp()
 
-// Function call to initialize app
-init();
--- README Cherie's file.md
+++ README Cherie's file.md
@@ -1,2 +1,30 @@
 # Cherie-Davis-ReadMe-07HW
 Module 7 Node Homework | Creating a Professional ReadMe Guide
+
+# Project Title
+
+## Description
+- **Motivation**: [User's motivation]
+- **Why build this project**: [User's reason]
+- **Problem solved**: [User's answer]
+- **What I learned**: [User's answer]
+
+## Table of Contents
+
+## Installation
+- [Tools or packages installed]
+
+## Usage
+- [Usage instructions]
+
+## Contributing
+- Collaborators: [User's answer]
+
+## License
+- License used: [User's answer]
+
+## Tests
+- [Whether or not tests were run]
+
+## Questions
+- Features: [User's features]
